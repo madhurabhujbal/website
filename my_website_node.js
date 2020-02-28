8b0ec2a046a58674885094df6cee75145b8c1454
Madhura <madhura.bhujbal@gmail.com>
2020-02-28 18:41:15 +0000

    Migrate home page to node, no css or images in it

--- my_website_node.js
+++ my_website_node.js
@@ -4,16 +4,15 @@ const path = require('path');
 
 //creating server
 http.createServer(function (req, res) {
-    // res.write('Hello world');
-    //    let filepath = path.join(__dirname, 'index.js');
-    //    res.write(filepath);
-function fileReadCallback(err, data) {
-    if (err) throw err
-    res.write(data);
-    res.end();
-    //console.log(data);
+    let filepath = path.join(__dirname, req.url === '/' ? "\\html\\home.html" : req.url);
+    let contentType = 'text/html';
+function fileReadCallback(err, content) {
+    if (err) return;
+    // res.write(data);
+    // res.end();
+    res.writeHead(200, {'Content-type': contentType});
+    res.end(content, 'utf8');
 }
-// fs.readFile(path.relative("C:\\my_website\\html\\home.html"), utf8, fileReadCallback)
-// fs.readFile(path.join(__dirname, 'css', 'custom.css'), 'utf8', fileReadCallback)
-fs.readFile(path.join(__dirname, 'html', 'home.html'), 'utf8', fileReadCallback)
+fs.readFile(filepath, 'utf8', fileReadCallback)
+console.log(req.url + '->' + filepath);
 }).listen(8080);

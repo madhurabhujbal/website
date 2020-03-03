ffab6a3fe6780efe69a6ee3e80b52ab07847d27a
Madhura <madhura.bhujbal@gmail.com>
2020-03-03 22:28:00 +0000

    Replace 8080 port by process environment port

--- test/test_my_website.js
+++ test/test_my_website.js
@@ -1,17 +1,19 @@
 var expect = require('chai').expect;
 var request = require('request');
 
+const PORT = process.env.PORT || 8080;
+
 describe('Status and content', function(){
     describe('Main page', function() {
         it ('status', function(done) {
-            request('http://localhost:8080/', function(error, response, body) {
+            request(`http://localhost:${PORT}/`, function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
                 done();
             });
         });
 
         it('content', function(done) {
-            request('http://localhost:8080/', function(error, response, body) {
+            request(`http://localhost:${PORT}/`, function(error, response, body) {
                 expect(body).to.contains('/images/fb_profile.jpg');
                 expect(body).to.contains('/files/cv.pdf');
                 done();

c1587429f9d1c7d4c2b101cb36ff63761052ae24
Madhura <madhura.bhujbal@gmail.com>
2020-06-08 13:02:00 +0100

    Add code for handling request for rendering about and hobbies page

--- my_website_node.js
+++ my_website_node.js
@@ -1,7 +1,5 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.set('views', __dirname + '/views/');
@@ -11,6 +9,14 @@ app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/hobbies', (req, res) => {
+    res.render('hobbies');
 });
 
 const PORT = process.env.PORT || 8080;

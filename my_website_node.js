4af42a8514e9d0307ce505a7380da9d3cd3502cd
Madhura <madhura.bhujbal@gmail.com>
2020-06-08 12:15:03 +0100

    Migrate code for rendering home page to express and take static files needed for the same from public folder

--- my_website_node.js
+++ my_website_node.js
@@ -7,49 +7,11 @@ const app = express();
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, '/public/')));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-// //creating server
-// const server = http.createServer(function (req, res) {
-//     let filepath = path.join(__dirname, req.url === '/' ? "/html/home.html" : req.url);
-
-//     //let filepath = path.join(__dirname, req.url === '/' ? "\\html\\home.html" : req.url);
-//     let extname = path.extname(filepath);
-//     console.log(req.url + ' -> ' + filepath + ' Ext:' + extname);
-
-//     let contentType = 'text/html';
-//     // if contentType is not declared and initialised here, data race condition will rise. It means
-//     //that when extname is checked in switch case, as this is asynchronous process,
-//     //till the time .css file is checked,
-//     //control moves to next case. If image with .jpg extention is found, the content type is set to image/jpg
-//     //even for the css file. Hence, css will not render in the requested url.
-//     switch(extname){
-//         case '.css':
-//             contentType = 'text/css';
-//         break;
-
-//         case '.jpg':
-//             contentType = 'image/jpg'
-//         break;
-
-//         case '.pdf':
-//             contentType = 'application/pdf'
-//         break;
-//     }
-
-//     function fileReadCallback(err, content) {
-//         if (err) return;
-//         // res.write(data);
-//         // res.end();
-//         res.writeHead(200, {'Content-type': contentType});
-//         res.end(content);
-//     }
-
-//     fs.readFile(filepath, fileReadCallback)
-//     // console.log(req.url + '->' + filepath);
-// });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
